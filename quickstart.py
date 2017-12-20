1acd7c2ac2b6b18d36cd120e48d7028225c2ac4e
eddyizm <eddyizm@gmail.com>
2017-12-19 17:57:09 -0800

    another change to quickstart

--- quickstart.py
+++ quickstart.py
@@ -13,20 +13,20 @@ session = InstaPy(username=insta_username, password=insta_password)
 session.login()
 
 # set up all the settings
-'''
-session.set_upper_follower_count(limit=500)
+
+session.set_upper_follower_count(limit=500)#
 session.set_lower_follower_count(limit = 50)
 session.set_do_comment(True, percentage=10)
 session.set_do_follow(enabled=True, percentage=10, times=2)
 session.set_comments(['Rad!', 'Super cool!', 'Excellent!', 'Amazing!'])
-'''
+
 # session.set_dont_include(['friend1', 'friend2', 'friend3'])
 session.set_dont_like(['death', 'cancer'])
 
 # do the actual liking
 # session.like_by_tags(['natgeo', 'world'], amount=100)
-session.like_by_feed(amount=200)
-session.like_from_image(amount=25)
-# session.like_by_tags(['bouldering', 'climbing_pictures_of_instagram', 'natgeo', 'optoutside'], amount=125 )
+# session.like_by_feed(amount=200)
+# session.like_from_image(amount=25)
+session.like_by_tags(['bouldering', 'climbing_pictures_of_instagram', 'natgeo', 'optoutside'], amount=125 )
 # end the bot session
 session.end()
