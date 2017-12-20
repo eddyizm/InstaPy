f56fe8c93378db066202762add7ffdb633887206
eddyizm <eddyizm@gmail.com>
2017-12-19 19:22:01 -0800

    added new scripts

--- bouldering.py
+++ bouldering.py
@@ -9,19 +9,21 @@ insta_password = login[1]
 
 # if you want to run this script on a server, 
 # simply add nogui=True to the InstaPy() constructor
-session = InstaPy(username=insta_username, password=insta_password)
+session = InstaPy(username=insta_username, password=insta_password, headless_browser=True)
 session.login()
 
 # set up all the settings
-session.set_upper_follower_count(limit=500)
+session.set_upper_follower_count(limit=500)#
 session.set_lower_follower_count(limit = 50)
+session.set_do_comment(True, percentage=10)
+session.set_do_follow(enabled=True, percentage=10, times=2)
+session.set_comments(['Rad!', 'Super cool!', 'Excellent!', 'Amazing!'])
 
 # session.set_dont_include(['friend1', 'friend2', 'friend3'])
 session.set_dont_like(['death', 'cancer'])
 
 # do the actual liking
-# session.like_by_tags(['natgeo', 'world'], amount=100)
-session.like_by_feed(amount=200)
-# session.like_from_image(amount=25)
+session.like_by_tags(['bouldering', 'climbing_pictures_of_instagram', 'natgeo', 'optoutside'], amount=125 )
+
 # end the bot session
 session.end()
--- thumbsup.py
+++ thumbsup.py
@@ -9,19 +9,22 @@ insta_password = login[1]
 
 # if you want to run this script on a server, 
 # simply add nogui=True to the InstaPy() constructor
-session = InstaPy(username=insta_username, password=insta_password)
+session = InstaPy(username=insta_username, password=insta_password,  headless_browser=True)
 session.login()
 
 # set up all the settings
 session.set_upper_follower_count(limit=500)
 session.set_lower_follower_count(limit = 50)
-
+session.set_do_comment(True, percentage=20)
+# session.follow_user_followers(['noel_russ'], amount=5, randomize=True)
+session.set_comments([u':clap:', u':thumbsup:', u':raised_hands:'])
 # session.set_dont_include(['friend1', 'friend2', 'friend3'])
 session.set_dont_like(['death', 'cancer'])
 
 # do the actual liking
-# session.like_by_tags(['natgeo', 'world'], amount=100)
-session.like_by_feed(amount=200)
-# session.like_from_image(amount=25)
+session.like_by_tags(['wanderlust', 'rei1440project', 'nationalpark'], amount=100)
+# session.like_by_tags(['gothefuckoutside', 'optoutside'], amount=50)
+
 # end the bot session
 session.end()
+
