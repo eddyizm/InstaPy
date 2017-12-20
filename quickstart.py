0b54602106dfb7c3ac5810ca087ffca7df0cbf72
eddyizm <eddyizm@gmail.com>
2017-12-19 17:56:45 -0800

    updated quickstart and git ignore.

--- quickstart.py
+++ quickstart.py
@@ -1,7 +1,11 @@
 from instapy import InstaPy
 
-insta_username = ''
-insta_password = ''
+# read login info from file
+f = open ('scripts/login.txt', 'r')
+login = f.read().splitlines()
+
+insta_username = login[0]
+insta_password = login[1]
 
 # if you want to run this script on a server, 
 # simply add nogui=True to the InstaPy() constructor
@@ -9,14 +13,20 @@ session = InstaPy(username=insta_username, password=insta_password)
 session.login()
 
 # set up all the settings
-session.set_upper_follower_count(limit=2500)
+'''
+session.set_upper_follower_count(limit=500)
+session.set_lower_follower_count(limit = 50)
 session.set_do_comment(True, percentage=10)
-session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-session.set_dont_include(['friend1', 'friend2', 'friend3'])
-session.set_dont_like(['pizza', 'girl'])
+session.set_do_follow(enabled=True, percentage=10, times=2)
+session.set_comments(['Rad!', 'Super cool!', 'Excellent!', 'Amazing!'])
+'''
+# session.set_dont_include(['friend1', 'friend2', 'friend3'])
+session.set_dont_like(['death', 'cancer'])
 
 # do the actual liking
-session.like_by_tags(['natgeo', 'world'], amount=100)
-
+# session.like_by_tags(['natgeo', 'world'], amount=100)
+session.like_by_feed(amount=200)
+session.like_from_image(amount=25)
+# session.like_by_tags(['bouldering', 'climbing_pictures_of_instagram', 'natgeo', 'optoutside'], amount=125 )
 # end the bot session
 session.end()
