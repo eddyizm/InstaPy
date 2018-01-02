9a492e1ad1e828f9e29b131cd3a0143bb381e983
eddyizm <eddyizm@gmail.com>
2018-01-01 18:18:13 -0800

    Added script to send stats by email.

---
+++ instaMail.py
@@ -0,0 +1,74 @@
+import smtplib
+import sqlite3 
+
+def SendBody(body, d):
+    # get login info
+    f = open ('scripts/login.txt', 'r')
+    login = f.read().splitlines()
+    f.close()
+    u = login[2]
+    l = login[3]
+    # set up the SMTP server
+    s = smtplib.SMTP(host='smtp.live.com', port=587)
+    s.starttls()
+    s.login(u, l)
+
+    # import necessary packages
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    # create a message
+    msg = MIMEMultipart()       
+    print('sending mail')
+      
+    # setup the parameters of the message
+    msg['From']='cervantes.e@outlook.com'
+    msg['To']='eddyizm@me.com'
+    msg['Subject']='Instapy Stats '+d
+
+    # add in the message body
+    msg.attach(MIMEText(body, 'plain'))
+
+    # send the message via the server set up earlier.
+    s.send_message(msg)
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+def daily_log():
+    #get follower count 
+    fCount = file_len('logs/followerNum.txt')
+    f = open('logs/followerNum.txt','r').readlines()
+    s = f[fCount-1]
+    followers = 'followers: '+(s[-5:]+'\n')
+    # Access DB and parse out data.
+    conn = sqlite3.connect('C:\\Users\\eddyizm\\Source\\Repos\\InstaPy\\db\\instapy.db')
+    # apparently you have to have a cursor with python/sqlite
+    c = conn.cursor()
+    # query text
+    q = 'SELECT likes, comments, follows, unfollows, server_calls, created FROM statistics WHERE created > date("now","-4 day");'
+    results = c.execute(q)
+    columns = 'like | comment | follow | unfollow | server_call | created'
+    day = c.fetchone()
+    z = '\n%s | %s | %s | %s | %s | %s' % (day[0],day[1],day[2], day[3], day[4], day[5])
+    nextday = c.fetchone()
+    x = '\n%s | %s | %s | %s | %s | %s' % (nextday[0],nextday[1],nextday[2], nextday[3], nextday[4], nextday[5])
+    # close db connection
+    conn.close() 
+    message =  followers+columns+x+z+'\n'
+
+    # get log info : 
+    endFile = file_len('logs/timelog.txt')
+    n = open('logs/timelog.txt','r')
+    log = n.readlines()
+    count = endFile-15
+    for x in range(count, (endFile)):
+        message+=log[x]
+    n.close()
+    # print(message)
+    SendBody(message, nextday[5])
+
+# os.system('pause')
