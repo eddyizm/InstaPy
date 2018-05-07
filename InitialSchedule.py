67c1a062e196f1d2f32345e13defab72dd512169
eddyizm <eddyizm@gmail.com>
2018-05-06 19:36:58 -0700

    updated quickstart yet again. failing on mac

--- InitialSchedule.py
+++ InitialSchedule.py
@@ -18,7 +18,7 @@ def sendlog():
 
 def quickstart():
     try:
-        f = open ( logintext , 'r')
+        f = open (logintext, 'r')
         login = f.read().splitlines()
         f.close()
         insta_username = login[0]
@@ -29,7 +29,7 @@ def quickstart():
         print ('pre login')     
         session.login()
         session.set_relationship_bounds(enabled=True,
-                potency_ratio=None,
+                potency_ratio=-None,
                 delimit_by_numbers=True,
                 max_followers=5000,
                     max_following=5555,
@@ -38,22 +38,12 @@ def quickstart():
         session.set_dont_like(['death', 'cancer'])
         session.like_by_feed(amount=100, unfollow=False, randomize=True, interact=False)
         instaMail.completeTask('quickstart success')
+
     except Exception as exc:
-        print('quickstart fail!')
-        instaMail.completeTask('quickstart fail!')
-        # # if changes to IG layout, upload the file to help us locate the change
-        # if isinstance(exc, NoSuchElementException):
-        #     file_path = os.path.join(gettempdir(), '{}.html'.format(time.strftime('%Y%m%d-%H%M%S')))
-        #     with open(file_path, 'wb') as fp:
-        #         fp.write(session.browser.page_source.encode('utf8'))
-        #     print('{0}\nIf raising an issue, please also upload the file located at:\n{1}\n{0}'.format(
-        #         '*' * 70, file_path))
-        # # full stacktrace when raising Github issue
-        #raise
+        print ('quickstart fail')
 
     finally:
         session.end()
-            
 
 def thumbsup():
     try:
