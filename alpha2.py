81a29f02f8a72f46eafe6af9be7587e7a305de61
eddyizm <eddyizm@gmail.com>
2018-05-08 06:47:56 -0700

    added alpha file to run independently

---
+++ alpha2.py
@@ -0,0 +1,75 @@
+from instapy import InstaPy
+import time
+import instaMail
+from selenium.common.exceptions import NoSuchElementException
+from tempfile import gettempdir
+import os
+
+
+if os.name == 'nt':
+    logintext = "C:\\Users\\eddyizm\\Desktop\\Work\\login.txt"
+else:
+    logintext = "/Users/eduardocervantes/Desktop/Macbook/login.txt"
+
+def alpha():
+    try:
+        print ('alpha script')
+        f = open ( logintext , 'r')
+        login = f.read().splitlines()
+        f.close()
+        insta_username = login[0]
+        insta_password = login[1]
+        session = InstaPy(username=insta_username, password=insta_password, headless_browser=True)
+        session.login()
+        session.set_dont_like(['death', 'cancer'])
+        session.set_relationship_bounds(enabled=True,
+                potency_ratio=None,
+                delimit_by_numbers=True,
+                max_followers=5000,
+                    max_following=5555,
+                    min_followers=45,
+                    min_following=77)
+        session.set_do_comment(True, percentage=30)
+        session.set_comments([u':clap:', u':thumbsup:', u':raised_hands:'])
+        session.like_by_tags(['wonderful_places', 'mavicpro', 'determination'], amount=50 )
+        print('alpha success')  
+        #instaMail.completeTask('alpha success')
+    except Exception as exc:
+        print('alpha fail!')        
+        #instaMail.completeTask('alpha fail')
+        # if changes to IG layout, upload the file to help us locate the change
+        #if isinstance(exc, NoSuchElementException):
+            # file_path = os.path.join(gettempdir(), '{}.html'.format(time.strftime('%Y%m%d-%H%M%S')))
+            # with open(file_path, 'wb') as fp:
+            #     fp.write(session.browser.page_source.encode('utf8'))
+            # print('{0}\nIf raising an issue, please also upload the file located at:\n{1}\n{0}'.format(
+            #     '*' * 70, file_path))
+        # full stacktrace when raising Github issue
+        
+    finally:
+        # end the bot session
+        session.end()
+       
+alpha()
+
+'''
+#photo #vlog # #myerasmus #dji #panasonic #engineer # #germany #sexy #creative #erasmus 
+#machupicchu  #waynapicchu #dbtravel #turkmen #turkmengirl  #blagoevgrad #bulgaria  #aubg #campus #2018 #senior #collegelife
+#artnude #nudeart #coutrylife #explorenewzealand #purenewzealand #gonewzealand
+#kilonewton #renewableenergy #engineering #greenenergy
+#functionalart  #textiles #artmatsdm #whimsical #charecterdesign
+#folkscenery #roamtheplanet #teamcanon #folkgreen #stayandwander 
+ #folkgoood  #agameoftones #keepitwild #roadtrippin #roadtrip #dreaming #earthpix #moodynature #exploremore #folkcreative  
+ #ingrande_crew #calabria #loves_united_calabria #loves_united_italia #volgocalabria 
+ #volgoitalia #ig_italia #igerscalabria #italia #calabriadaamare #like4like #vivocalabria #like4follow 
+ #verso_sud #yallerscalabria #follow4follow #yallersitalia #italia_inunoscatto #likeforlike
+ #italiainunoscatto_hdr #ig_calabria #ig_crotone #igreggiocalabria #instanlikes #igcosenzan
+ #underwater  #landscape_captures #followback #ig_europe #topfrancephoto 
+ #ig_worldclub #followbackinstantly #followme #super_europe #igworldclub 
+ # #follow4follow #nature_perfection #worldtravelpics
+ #igphoto #igersoftheday #igersworldwide #ig_captures #igs_photos #ig_daily #worldbestgram #splendid_shotz
+ #proud #lol #smile #snapback #headuphigh #pushthroughit #igotthis 
+ #almostthere # #fit #fitnessfreaks #fit#fitnessmotivation # #healthy #tone 
+ #tanlines #cutoffs #countrylife #farm #liveamplified
+ #noh8 #blessed #keeppushing #keepgoing #keepgrinding #getit #blah #icute #summertime
+ '''
