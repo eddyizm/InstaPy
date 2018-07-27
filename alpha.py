23d673019e878cdfdef11dd0b7831b3dd4952246
eddyizm <eddyizm@gmail.com>
2018-07-27 07:15:34 -0700

    added more tags

--- alpha.py
+++ alpha.py
@@ -60,23 +60,5 @@ if __name__ == '__main__':
     alpha()
 
 '''
-#photo #vlog #mavicpro #myerasmus #dji # #engineer # #germany  #creative #erasmus 
- #dbtravel #turkmen #  #blagoevgrad  #aubg #campus #2018 #senior #collegelife
-# # # #explorenewzealand #purenewzealand #gonewzealand
-#kilonewton #renewableenergy #engineering #greenenergy
-#functionalart  # #artmatsdm  #charecterdesign
-# #roamtheplanet #teamcanon #folkgreen  
- #folkgoood  # # #roadtrippin  #dreaming # #moodynature #exploremore #  
- #ingrande_crew #calabria #loves_united_calabria #loves_united_italia #volgocalabria 
- #volgoitalia #ig_italia #igerscalabria #italia #calabriadaamare #like4like #vivocalabria #like4follow 
- #verso_sud #yallerscalabria #follow4follow #yallersitalia #italia_inunoscatto #likeforlike
- #italiainunoscatto_hdr #ig_calabria #ig_crotone #igreggiocalabria # #igcosenzan
- #underwater  #landscape_captures #followback #ig_europe #topfrancephoto 
- #ig_worldclub #followbackinstantly #followme # #igworldclub 
- # # #nature_perfection #
- #igphoto # #igersworldwide #ig_captures #igs_photos #ig_daily #worldbestgram #splendid_shotz
- #proud #lol #smile #snapback # #pushthroughit #igotthis 
- #almostthere #determination #fit  #tone 
- #tanlines # # #farm 
- #noh8 #blessed #keeppushing #keepgoing #keepgrinding #getit #blah #icute #summertime
+
  '''
