cfef6a59e82a32d7f2160c4ec16af2556d400844
eddyizm <eddyizm@gmail.com>
2018-10-28 11:57:24 -0700

    updated from upstream and changed some tags

--- alpha2.py
+++ alpha2.py
@@ -32,7 +32,7 @@ def alpha():
                     min_following=77)
         session.set_do_comment(True, percentage=30)
         session.set_comments([u':clap:', u':thumbsup:', u':raised_hands:'])
-        session.like_by_tags(['traveldiary', 'russianmodel', 'guangzhou'], amount=50 )
+        session.like_by_tags(['Mountaincrushmonday', 'sexypics', 'finland'], amount=50 )
         print('alpha2 success')  
         instaMail.completeTask('alpha2 success')
     except Exception as exc:
@@ -62,7 +62,7 @@ alpha()
 #domenica #sundaymood  #mylife
 #ilmiocuoresoloperte #aroundtheworld   #noidue #noi #instagramstories #instagramood #instagram'
  #swimming  #grancenote  #tulum  #tb #traveling # #travel #vacation # #cave #cenote #beautiful #water #motivacnystatusalways'
-#IBTMscout #nextmodels #WLYG #imgirls #newyork #numa #wescoutusa #m4scouting #LA #manager #model #fashion #filmmaker #cast #director #italy   #photographer # #acting #shortfilm #colombia #bloger #beauty #makeup #magazine #trends #clothes #paris'
+#IBTMscout # #WLYG #imgirls #newyork #numa #wescoutusa #m4scouting #LA #manager #model #fashion #filmmaker #cast #director #italy   #photographer # #acting #shortfilm #colombia #bloger #beauty #makeup #magazine #trends #clothes #paris'
 # #nature # #exploring #enjoyinglife #croatiafulloflife' 
  #vilnius #guangzhoutower #guangzhoucity #thehide # # #shanghai #shenzhen #milan #dubai #jeddah # #foodporn #like4like #tokyo 
 #vilniusoldtown #kiev #odessa #hangzhou #foshan #dongguan #worldcup2018 #seoul #brasiliangirl #panyu #guangzhoufood #brasilianmodel 
@@ -70,8 +70,8 @@ alpha()
 # #love #thirdeye # #higherconsciousness #success #gratitude #positivethinking #loa #thesecret #spiritual 
 #sannyday#summer#summer2017#see#beach#batumi#batumi2017#batumicity#batumibeach#georgia#georgiagirl##russiangirl#followme#follow#followers#weekend#face'
  b'#uncoveredmagazine #sensual_trophy #sensual_dreams #boudoirphotography #sensuality_bnw #sensuality_world #sensual_ladies #infinity_sense #sensualart_flair 
-#igf_sensual2 #lamalaeducacion #digers_body #shotsosensual #seduction_sensuality #be_one_sensual #sexypics #my_secret_lover_ #great_captures_sensual #sexylingerie #beautyandboudoir #best_expression_sensuality 
-#loves_sensual_mood #pizza #insta_legs_2015 #pizzadreams #hot #hotgirls #fit4life #fitbody #fitlove'   
+#igf_sensual2 #lamalaeducacion #digers_body #shotsosensual #seduction_sensuality #be_one_sensual # #my_secret_lover_ #great_captures_sensual #sexylingerie #beautyandboudoir #best_expression_sensuality 
+#loves_sensual_mood #pizza #insta_legs_2015 #pizzadreams #hot #hotgirls  #fitbody #fitlove'   
 '#haryanvi #haryanvistatus #haryanvi #haryanviculture #haryana #models #modelling #modellife #model #medellin #modello #italy # 
 #haryanvi #mood #modern #musically #manjul #handsome #handsomehunk #tag  #cool #dashing #supermodel #actor #upcomingmodel  #indianmodel #indianmodels #indianfollowers'
 #ddlg #gls #tattoos #alternative #  #toes #vintage #feet  #fetish #outdoorshoot  #bathtime #fitfam #lingerie #pinup #freckles #fashionmodel  #pinkhair  #alt #kink #longlegs #girlswithtattoos #art #azmodel  #travelingmodel
@@ -91,14 +91,14 @@ alpha()
  #ingrande_crew #calabria #loves_united_calabria #loves_united_italia #volgocalabria 
  #volgoitalia #ig_italia # #italia #calabriadaamare  #vivocalabria #like4follow 
  #verso_sud #yallerscalabria #follow4follow #yallersitalia #italia_inunoscatto #likeforlike
- # #ig_calabria #ig_crotone #igreggiocalabria #instanlikes #igcosenzan
+ #ig_calabria #ig_crotone #igreggiocalabria #instanlikes #igcosenzan
  #landscape_captures #followback # #topfrancephoto 
  #ig_worldclub # #followme #super_europe 
  # #follow4follow #nature_perfection #worldtravelpics
- #igphoto # #igersworldwide #ig_captures #igs_photos #ig_daily #worldbestgram #splendid_shotz
- #proud #lol #smile #snapback # #pushthroughit #igotthis 
+ #igphoto #igersworldwide #ig_captures #igs_photos #ig_daily #worldbestgram #splendid_shotz
+ #snapback #pushthroughit #igotthis 
  #almostthere # #fit #fitnessfreaks #fit#  #healthy #tone 
- #tanlines #cutoffs #countrylife #farm #
+ #tanlines #cutoffs #countrylife #farm
  #noh8 #blessed #keeppushing #keepgoing   #blah #icute #summertime
   #artnude  #sensual_art # #longhair ##sexypose #  #passion  #bodypositivity #ass'
  '''
